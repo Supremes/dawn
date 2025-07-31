629f83525da2dace4c383674665e788b33126c39
Supremes <1017052447@qq.com>
2025-07-31 20:57:52 +0800

    remove unuseful folder

---
+++ release/scripts/backend_restart.sh
@@ -0,0 +1,7 @@
+SOURCE_PATH=$PWD
+cd ../../aurora-springboot
+mvn package
+cd $SOURCE_PATH
+cp ../aurora-springboot/target/aurora-springboot-0.0.1.jar .
+docker compose up -d --build fatcat
+echo "成功重新创建并运行主程序fatcat"
