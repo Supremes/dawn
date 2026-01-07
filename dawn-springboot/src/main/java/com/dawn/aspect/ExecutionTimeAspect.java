6d3c898bdf2c3d927df3039f4248f998529c5924
Supremes <1017052447@qq.com>
2026-01-08 00:02:55 +0800

    配置JVM grafana+prometheus 监控

--- dawn-springboot/src/main/java/com/dawn/aspect/ExecutionTimeAspect.java
+++ dawn-springboot/src/main/java/com/dawn/aspect/ExecutionTimeAspect.java
@@ -1,5 +1,8 @@
 package com.dawn.aspect;
 
+import com.dawn.constant.MetricsConstant;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -17,11 +20,22 @@ import java.util.Objects;
 @Slf4j
 @Component
 public class ExecutionTimeAspect {
+    private final MeterRegistry meterRegistry;
+
+    ExecutionTimeAspect(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
     @Pointcut("execution(* com.dawn.controller.*.*(..))")
     public void executionPointCut() {}
 
     @Before("executionPointCut()")
     public void executionBefore(){
+        Counter.builder(MetricsConstant.REQUEST_COUNT)
+                .description("Controller 请求数")
+                .tag("status", "all")
+                .register(meterRegistry)
+                .increment();
         log.debug("Logging before method execution");
     }
 
@@ -34,6 +48,27 @@ public class ExecutionTimeAspect {
         ApiOperation apiOperation = method.getAnnotation(ApiOperation.class);
         String desc = (apiOperation != null) ? apiOperation.value() : "No description";
         log.debug("Method URI: {} , Method name: {}, Swagger Desc: {}, returning with : {}", Objects.requireNonNull(request).getRequestURI(), method.getName(), desc, result);
+
+        Counter.builder(MetricsConstant.REQUEST_OK_COUNT)  // 指标名称
+                .description("Controller 请求成功总数")
+                .tag("method_name", method.getName())    // 标签1：按方法名分类
+                .tag("uri", Objects.requireNonNull(request).getRequestURI())                   // 标签2：按 URI 分类
+                .tag("status", "success")          // 标签3：标记为成功
+                .register(meterRegistry)           // 注册到容器
+                .increment();                      // 计数 +1
+    }
+
+    @AfterThrowing(pointcut = "executionPointCut()", throwing = "e")
+    public void countException(JoinPoint joinPoint, Throwable e) {
+        String methodName = joinPoint.getSignature().getName();
+
+        Counter.builder(MetricsConstant.REQUEST_EXCEPTION_COUNT) // 使用相同的指标名
+                .description("Controller 请求异常总数")
+                .tag("method_name", methodName)
+                .tag("status", "error")           // 标签不同：标记为失败
+                .tag("exception", e.getClass().getSimpleName()) // 记录是什么异常
+                .register(meterRegistry)
+                .increment();
     }
 
 }
